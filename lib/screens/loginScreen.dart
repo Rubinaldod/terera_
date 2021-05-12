8d7d65ee591d5bafc9f63d8170884d682b7f10c1
Flowers11sana <sanak@africau.edu>
2021-05-12 22:56:34 +0200

    I put hello world

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text("Hello world"),
+    );
   }
 }
