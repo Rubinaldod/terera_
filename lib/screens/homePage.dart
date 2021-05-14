1d65bebf947a2919d5ca4bc3ecbac7bf6e116d10
Flowers11sana <sanak@africau.edu>
2021-05-14 11:36:07 +0200

    Homepage almost done

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -28,7 +28,7 @@ class _HomePageState extends State<HomePage> {
                             child: Container(
                               margin: EdgeInsets.only(left: 20),
                               child: Text(
-                                'Logo',
+                                'Terera',
                                 style: TextStyle(color: Colors.white),
                               ),
                             ),
@@ -36,10 +36,11 @@ class _HomePageState extends State<HomePage> {
                           Expanded(
                             flex: 1,
                             child: Container(
-                                child: Icon(
-                              Icons.compass_calibration_sharp,
-                              size: 25,
-                            )),
+                              child: Icon(
+                                Icons.compass_calibration_sharp,
+                                size: 25,
+                              ),
+                            ),
                           ),
                           Expanded(
                             flex: 1,
@@ -184,48 +185,198 @@ class _HomePageState extends State<HomePage> {
                             color: Colors.white24,
                             borderRadius: BorderRadius.circular(10),
                           ),
-                          child: Stack(
-                            children: <Widget>[
-                              Container(
-                                width: 90,
-                                height: 170,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage('images/nasty_C.jpg'),
-                                    fit: BoxFit.cover,
+                          child: Row(
+                            children: [
+                              Stack(
+                                children: <Widget>[
+                                  Container(
+                                    width: 90,
+                                    height: 170,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage('images/nasty_C.jpg'),
+                                        fit: BoxFit.cover,
+                                      ),
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
                                   ),
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
+                                  Container(
+                                    margin: EdgeInsets.only(left: 20),
+                                    width: 90,
+                                    height: 170,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage('images/nasty_C.jpg'),
+                                        fit: BoxFit.cover,
+                                      ),
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(left: 40),
+                                    width: 90,
+                                    height: 170,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage('images/nasty_C.jpg'),
+                                        fit: BoxFit.cover,
+                                      ),
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              Container(
-                                margin: EdgeInsets.only(left: 20),
-                                width: 90,
-                                height: 170,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage('images/nasty_C.jpg'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(left: 40),
-                                width: 90,
-                                height: 170,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage('images/nasty_C.jpg'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(10),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        alignment:
+                                            AlignmentDirectional.topStart,
+                                        margin:
+                                            EdgeInsets.only(top: 8, left: 10),
+                                        child: Text(
+                                          'SEE WHO YOU LISTENED TO IN 2020',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        alignment:
+                                            AlignmentDirectional.topStart,
+                                        margin:
+                                            EdgeInsets.only(top: 8, left: 10),
+                                        child: Text(
+                                          'Your top artists, songs, podcasts of the year and more..',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.normal,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
                           ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      child: Container(
+                        alignment: AlignmentDirectional.topStart,
+                        margin: EdgeInsets.only(top: 8, left: 10),
+                        child: Text(
+                          'Popular',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 23,
+                          ),
+                        ),
+                      ),
+                      height: 40,
+                    ),
+                    Container(
+                      height: 170,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            topScroll(),
+                            topScroll(),
+                            topScroll(),
+                            topScroll(),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      child: Container(
+                        alignment: AlignmentDirectional.topStart,
+                        margin: EdgeInsets.only(top: 8, left: 10),
+                        child: Text(
+                          'FEATURED',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 23,
+                          ),
+                        ),
+                      ),
+                      height: 40,
+                    ),
+                    Container(
+                      height: 170,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/davido.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(60.0),
+                        child: Container(
+                          width: 70,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(color: Colors.white, width: 2),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                              child: Text(
+                                'CHECK IT OUT',
+                                style: TextStyle(
+                                  letterSpacing: 1,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      child: Container(
+                        alignment: AlignmentDirectional.topStart,
+                        margin: EdgeInsets.only(top: 8, left: 10),
+                        child: Text(
+                          'NEW RELEASES',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 23,
+                          ),
+                        ),
+                      ),
+                      height: 40,
+                    ),
+                    Container(
+                      height: 170,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            topScroll(),
+                            middleScroll(),
+                            topScroll(),
+                            middleScroll(),
+                          ],
                         ),
                       ),
                     ),
--- lib/widgets/topScroll.dart
+++ lib/widgets/topScroll.dart
@@ -8,41 +8,42 @@ class topScroll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(left: 10),
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/nasty_C.jpg'),
-              fit: BoxFit.cover,
-            ),
-            borderRadius: BorderRadius.circular(20)),
-        width: 150,
-        child: Container(
-          margin: EdgeInsets.only(top: 100, left: 10),
-          child: Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'DARK',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Released Jan 29',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal),
-                  )
-                ],
-              )
-            ],
+      margin: EdgeInsets.only(left: 10),
+      decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/nasty_C.jpg'),
+            fit: BoxFit.cover,
           ),
-        ));
+          borderRadius: BorderRadius.circular(20)),
+      width: 150,
+      child: Container(
+        margin: EdgeInsets.only(top: 100, left: 10),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'DARK',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Released Jan 29',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
